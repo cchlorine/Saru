ec7b12eb93d970c27b9d591538ac270d7aee824e
lingoys <747284706@qq.com>
2014-04-27 08:27:18 +0800

    KunrFM Init

---
+++ js/main.js
@@ -0,0 +1,264 @@
+$(document).ready(function(){
+	//打开关闭播放列表
+	listStatus = 0;
+	$('.list-button').click(function(){
+		listWidth = $('#wrap .play-list ul').width();
+		if(!listStatus){
+			$(this).animate({marginRight: listWidth - 5},300);
+			$('#wrap .play-list ul').animate({marginRight: 0},300);
+			listStatus = 1;
+		} else {
+			$(this).animate({marginRight: -5},300);
+			$('#wrap .play-list ul').animate({marginRight: -400},300);
+			listStatus = 0;
+		}	
+	})
+	//console.log(localStorage.currentMusic + '-' + localStorage.repeat + '-' + localStorage.quality);
+	//加载本地数据（如果有的话）
+	if(typeof(localStorage.repeat) == undefined || isNaN(localStorage.repeat))
+		localStorage.repeat = '0';
+	if(typeof(localStorage.quality) == undefined || isNaN(localStorage.quality))
+		localStorage.quality = '0';
+	if(typeof(localStorage.prevplay) == undefined || isNaN(localStorage.prevplay))
+		localStorage.prevplay = '-1';
+	if(typeof(localStorage.prevplay) == currentMusic || isNaN(localStorage.currentMusic) || !playlist[localStorage.currentMusic])
+		localStorage.currentMusic = '0';
+	//localStorage.currentMusic = 0;
+	//各种需要用到的变量
+	var audio = document.getElementById('music');
+	var isPlaying    = false;
+	var currentMusic = localStorage.currentMusic;
+	var repeat = parseInt(localStorage.repeat);
+	var quality = 0;
+	var relist = ['fa-random', 'fa-refresh', 'fa-retweet'];
+	var retitle = ['Random', 'Cycle', 'Order'];
+
+	//根据状态修改图标
+	if(!quality){
+		$('.control .quality i').removeClass('fa-star').addClass('fa-star-half').attr('title','Normal Quality'); 
+	} else {
+		$('.control .quality i').removeClass('fa-star-half').addClass('fa-star').attr('title','High Quality'); 
+	}
+	$('.control .repeat i').removeClass().addClass('fa').addClass(relist[repeat]).attr('title',retitle[repeat]); 
+
+	console.log('Current Music: ' + currentMusic + ' Repeat: '+ repeat + ' Quality: '+ quality);
+
+	//加载播放列表
+	for (var i = 0; i < playlist.length; i++){
+		var item = playlist[i];
+		$('.play-list ul').append('<li class="item' + i + '">' + item.title + '</li>');
+	}    
+	
+	/**
+	* Request notification permissions
+	*/
+	function request_permission()
+	{
+		// 0 means we have permission to display notifications
+		
+	}
+	//载入图片和音乐文件
+	var loadMusic = function(i){
+		if(localStorage.currentMusic > -1)
+		localStorage.setItem('prevplay', localStorage.currentMusic * 1);
+		currentMusic = localStorage.currentMusic = i;
+		var item = playlist[i];
+
+		$.ajax({
+			type: "POST",
+			cache: false,
+		    dataType: 'jsonp',
+		    jsonp: 'callback',
+		    async: false,
+		    url: 'http://kunr.aliapp.com/xiami.php?id=' + item.id + '&type=song&callback=?',
+		    success: function(data) {
+				//选择不同质量的音乐文件
+		    	if(!quality){
+					src = item.mp3_h;
+				} else {
+					src = data.song_src;
+				}
+				src = data.song_src;
+				//加载各种东西
+				audio.setAttribute("src", src);
+				audio.addEventListener('play', playEvent, false);
+				audio.addEventListener('pause', stopEvent, false);
+				audio.addEventListener('timeupdate', updateProgress, false);
+				audio.addEventListener('ended', autoChange, false);
+				cover = data.song_cover;
+				cover = cover.replace('_1','_4');
+				cover = cover.replace('_2','_4');
+				cover = cover.replace('_3','_4');
+				cover = cover.replace('_4','_4');
+				cover = cover;
+				if(!cover)
+					cover = 'img/album.jpg';
+				$('.album img').attr({'src': cover, 'alt': item.artist});
+				$('#wrap .title h1').html(item.title);
+				$('#wrap .title h2').html(item.artist);
+				$('.play-list ul li').removeClass('playing').eq(i).addClass('playing');
+				
+				audio.play();
+
+				if(window.webkitNotifications && window.webkitNotifications.checkPermission() == 0){
+					var information = window.webkitNotifications.createNotification(cover, item.title, item.artist);
+					information.onclick = function(){
+						window.focus();
+						information.cancel();
+					}
+					information.show(); 
+					setTimeout(function(){information.cancel()},2000);
+				}
+				console.log('Song Title: ' + item.title + ' Song Artist: ' + item.artist);
+		    },
+		});	
+	}
+	loadMusic(currentMusic);
+	
+	var changeMusic = function(i){
+		loadMusic(i);
+		audio.play();
+	}
+
+	var randomNum = function(min,max){
+		var radx = Math.floor(min + Math.random() * (max - min));
+		if(radx != localStorage.currentMusic)
+			return radx;
+		else
+			return Math.floor(min + Math.random() * (max - min));
+	}
+
+	/*'fa-random', 'fa-refresh', 'fa-retweet'*/
+	var autoChange = function(){
+		audio.pause();
+		var nextMusic = 0;
+		switch(repeat){
+			case 0: nextMusic = randomNum(0, playlist.length); 
+				changeMusic(nextMusic); 
+				break;
+			case 1: audio.currentTime = 0.0;
+				audio.play();
+				break;
+			case 2: if(currentMusic == playlist.length - 1){
+					changeMusic(0);
+				} else {
+					changeMusic(currentMusic + 1);
+				} 
+				break;
+		}
+	}
+
+	var updateProgress = function(){
+		$('#wrap .progress .current').css({'width': audio.currentTime / audio.duration * 100 + '%'});
+	}
+	
+	var playEvent = function(){
+		$('.album').addClass('playing');
+		$('#wrap .progress').animate({opacity:"1"});
+		$('.start i').addClass('playing').removeClass('fa-play').addClass('fa-pause');
+		isPlaying = true;
+	}
+	
+	var stopEvent = function(){
+		$('.album').removeClass('playing');
+		$('.start i').removeClass('playing').removeClass('fa-pause').addClass('fa-play');
+		isPlaying = false;
+	}
+	
+	$('.center').click(function(){
+		if ($('.start i').hasClass('playing')){
+			audio.pause();
+		} else {
+			audio.play();
+			if(window.webkitNotifications){
+				if (window.webkitNotifications.checkPermission() != 0) {
+					window.webkitNotifications.requestPermission();
+				}
+			}
+		}
+	});
+
+	$('.control .pre').click(function(){
+		audio.pause();
+		if((currentMusic * 1 + 1) != localStorage.prevplay && localStorage.prevplay > -1){
+			changeMusic(localStorage.prevplay);
+		} else if(currentMusic == 0){
+			changeMusic(playlist.length - 1);
+		} else {
+			changeMusic(currentMusic - 1);
+		}
+	})
+
+	$('.control .next').click(function(){
+		audio.pause();
+		if(localStorage.repeat == 0){
+			nextMusic = randomNum(0, playlist.length); 
+			changeMusic(nextMusic); 
+		} else if(currentMusic == playlist.length - 1){
+			changeMusic(0);
+		} else {
+			changeMusic(currentMusic * 1 + 1);
+		}
+	})
+
+	$('.play-list ul li').click(function(){
+		if(!$(this).hasClass('playing')){
+			var className = $(this).attr('class');
+			var num       = parseInt(className.substr(4));
+			$('#wrap .list-button').animate({marginRight: -5},300);
+			$('#wrap .play-list ul').animate({marginRight: -400},300);
+			listStatus = 0;
+			audio.pause();
+			changeMusic(num);
+		}
+	})
+
+	$('.control .repeat').click(function(){
+		console.log('repeat ' + repeat);
+		if(repeat == 2){
+			$('.control .repeat i').removeClass(relist[repeat]).addClass(relist[0]).attr('title',retitle[0]); 
+			repeat = localStorage.repeat = 0;
+		} else {
+			$('.control .repeat i').removeClass(relist[repeat]).addClass(relist[repeat + 1]).attr('title',retitle[repeat + 1]); 
+			repeat = localStorage.repeat = repeat + 1;
+		}	
+	})
+	
+	$('.control .quality').click(function(){
+		if(quality){
+			$('.control .quality i').removeClass('fa-star').addClass('fa-star-half').attr('title','Normal Quality'); 
+			quality = localStorage.quality = 0;
+		} else {
+			$('.control .quality i').removeClass('fa-star-half').addClass('fa-star').attr('title','High Quality'); 
+			quality = localStorage.quality = 1;
+		}	
+	})
+	
+	$('.player').click(function(){
+		if(listStatus){
+		$('#wrap .list-button').animate({marginRight: -5},300);
+		$('#wrap .play-list ul').animate({marginRight: -400},300);
+		listStatus = 0;
+		}
+	})
+	
+	$('.setting').click(function(){
+		
+	})
+	
+	$('.iniciel_control').click(function(){
+			if($('.iniciel_control i').hasClass('fa-plus')){
+				$('.iniciel_control i').removeClass('fa-plus');
+				$('.iniciel_control i').addClass('fa-minus');
+			} else {
+				$('.iniciel_control i').removeClass('fa-minus');
+				$('.iniciel_control i').addClass('fa-plus');
+			}
+			$('#wrap .control').toggle(300);
+		}
+	)
+	
+	$('.home').click(function(){
+		window.open('http://www.dsu.pw');
+	})
+})
