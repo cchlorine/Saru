8e1b930f80fe9f7cb81cf281c8089583e73b28c5
Kunr <i@fake.moe>
2014-12-30 22:22:38 +0800

    类化代码

--- js/main.js
+++ js/main.js
@@ -1,43 +1,232 @@
-$(document).ready(function(){
+/* 本地数据初始化 */
 
-	/* 本地数据初始化 */
+if(typeof(localStorage.SaruQuality) == 'undefined')
+	localStorage.SaruQuality = 0;
 
-	if(typeof(localStorage.SaruQuality) == "undefined")
-		localStorage.SaruQuality = 0;
+if(typeof(localStorage.SaruRepeat) == 'undefined')
+	localStorage.SaruRepeat = 0;
 
-	if(typeof(localStorage.SaruRepeat) == "undefined")
-		localStorage.SaruRepeat = 0;
+if(typeof(localStorage.SaruLike) == 'undefined')
+	localStorage.SaruLike = '[]';
 
-	if(typeof(localStorage.SaruLike) == "undefined")
-		localStorage.SaruLike = '[]';
+var Saru = {
+	audio: document.getElementById('music'),
+	Prev: -1,
+	Current: 0,
+	isPlaying: false,
+	Like: JSON.parse(localStorage.SaruLike),
+	Repeat: {
+		Mode: parseInt(localStorage.SaruRepeat),
+		List: ['fa-random', 'fa-refresh', 'fa-retweet'],
+		Title: ['Random', 'Cycle', 'Order']
+	},
+	Quality: parseInt(localStorage.SaruQuality),
 
-	/* 常量 */
+	/* 初始化 */
+	Init: function() {
+		// 循环模式设置
+		$('.control .repeat i').addClass(this.Repeat.List[this.Repeat.Mode]).attr('title', this.Repeat.Title[this.Repeat.Mode]);
 
-	var audio     = document.getElementById('music'),
-			isPlaying = true,
-			SaruData  = {
-				'current' : 0,
-				'prev'    : -1,
-				'quality' : parseInt(localStorage.SaruQuality),
-				'pmode'   : parseInt(localStorage.SaruRepeat)
-			},
-			SaruLike = JSON.parse(localStorage.SaruLike);
+		/*if (location.hash.match(/[\d]+/)) {
+			var hash = location.hash.match(/[\d]+/)[0];
+			if (hash < playlist.length)
+				Saru.Load(hash);
+		} else*/
+		if (localStorage.SaruPrev > -1 && localStorage.SaruPrev < playlist.length) {
+			this.Load(localStorage.SaruPrev);
+		} else {
+			this.Load(this.extFunc.randNum(0,playlist.length));
+		}
+	},
 
-	/* 播放顺序 */
+	/* 控制 */
+	Control: {
+		Prev: function() { // 上一首
+			Saru.audio.pause();
 
-	var relist  = ['fa-random', 'fa-refresh', 'fa-retweet'],
-			retitle = ['Random', 'Cycle', 'Order'];
+			if ((+Saru.Current + 1) === Saru.Prev && Saru.Prev > -1) {
+				Saru.Load(Saru.Prev);
+			} else if (+Saru.Current == 0){
+				Saru.Load(playlist.length - 1);
+			} else if (Saru.Current == playlist.length) {
+				Saru.Load(Saru.Current);
+			} else {
+				Saru.Load(Saru.Current - 1);
+			}
+		},
 
-	$('.control .repeat i').addClass(relist[SaruData['pmode']]).attr('title',retitle[SaruData['pmode']]);
+		Next: function() { // 下一首
+			Saru.audio.pause();
+			var nextMusic = 0;
 
-	for (var i = 0; i < playlist.length; i++){
-		var item = playlist[i];
-		$('.play-list ul').append('<li class="item' + i + '">' + item.title + ' - ' + item.artist + '</li>');
-	}
+			switch(Saru.Repeat.Mode) {
+				case 0: // 随机播放
+				default:
+					Saru.Load(Saru.extFunc.randNum(0, playlist.length));
+					break;
+
+					case 1: // 单曲循环
+					audio.currentTime = 0.0;
+					audio.play();
+					break;
+
+				case 2: // 列表顺序
+					if (Saru.Current == playlist.length - 1){
+						Saru.Load(0);
+					} else {
+						Saru.Load(Saru.Current + 1);
+					}
+					break;
+			}
+		},
+
+		PlayMode: function() { // 播放顺序
+			if(Saru.Repeat.Mode == 2){
+				$('.control .repeat i').removeClass(Saru.Repeat.List[Saru.Repeat.Mode]).addClass(Saru.Repeat.List[0]).attr('title', Saru.Repeat.Title[0]);
+				Saru.Repeat.Mode = localStorage.SaruRepeat = 0;
+			} else {
+				$('.control .repeat i').removeClass(Saru.Repeat.List[Saru.Repeat.Mode]).addClass(Saru.Repeat.List[Saru.Repeat.Mode + 1]).attr('title', Saru.Repeat.Title[Saru.Repeat.Mode + 1]);
+				Saru.Repeat.Mode = localStorage.SaruRepeat = Saru.Repeat.Mode + 1;
+			}
+		}
+	},
+
+	/* 播放事件 */
+	Event: {
+		Play: function() { // 播放事件
+			$('#player').addClass('playing');
+			$('.start i').removeClass('fa-play').addClass('fa-pause');
+		},
+
+		Stop: function() { // 暂停事件
+			$('#player').removeClass('playing');
+			$('.start i').removeClass('fa-pause').addClass('fa-play');
+		},
+
+		UpdateProgress: function() { // 进度条更新事件
+			$('#progress .current').css({'width': Saru.audio.currentTime / Saru.audio.duration * 100 + '%'});
+		},
+
+		End: function() { // 结束事件
+			Saru.Control.Next();
+		}
+	},
+
+	/* 音质处理 */
+	Exp: {
+		Normal: function(v){
+			if (typeof v.type == undefined)
+				return false;
+
+				switch (v.type) {
+					case 'xiami':
+						$.ajax({
+							type: "GET",
+							cache: false,
+							dataType: 'jsonp',
+							jsonp: 'callback',
+							async: false,
+							url: 'http://www.xiami.com/song/playlist/id/' + v.source +'/object_name/collect/object_id/' + v.source + '/cat/json?_ksTS=1&callback=?',
+							success: function(data){
+								var item = playlist[v.id];
+
+								if (!data)
+									Saru.Control.Next();
+
+								if (!item.cover)
+									playlist[v.id]['cover'] = data['data']['trackList'][0]['pic'];
+
+								Saru.Play({id: v.id, source: Saru.extFunc.decodeXiami(data['data']['trackList'][0]['location'])});
+							}
+						});
+						break;
+
+					case '163':
+						AV.initialize("fvyglppby9vbe7x772p7r8ddrxzcq2tcljkxyrf9fo5spbfr", "fspuitd5h1fz3fftya2go8fg293fpwthivno5vjdic2cmwz4");
+						AV.Cloud.run('get163', {id: v.source, type: "song"}, function (result) {
+
+						var data = JSON.parse(result)['songs'][0],
+							item = playlist[v.id];
+
+							if (!data)
+								Saru.Control.Next();
+
+							if (!item.cover)
+								playlist[v.id]['cover'] = data['blurPicUrl'];
+
+							Saru.Play({id: v.id, source: data['mp3Url']});
+						});
+						break;
+
+					case 'file':
+						// Saru_ExpTest(v);
+						break;
+			}
+		}
+	},
+
+	/* 音乐播放 */
+	Play: function(ritem) {
+		var item = playlist[ritem.id];
+
+		// 记录
+		this.Prev    = localStorage.SaruPrev    = this.Current;
+		this.Current = localStorage.SaruCurrent = item.id;
+		location.hash = '#!' + item.id;
+
+		this.audio.setAttribute('src', ritem['source']);
+		this.audio.addEventListener('play',  this.Event.Play, false);
+		this.audio.addEventListener('pause', this.Event.Stop, false);
+		this.audio.addEventListener('timeupdate', this.Event.UpdateProgress, false);
+		this.audio.addEventListener('ended', this.Event.End, false);
+
+		// 设置封面
+		cover = item['cover'] ? item['cover'] : 'img/album.jpg';
+
+		$('#cover').attr({
+			'style': 'background-image:url(' + cover + ');',
+			'title': item['title'] + ' - ' + item['artist']
+		});
+
+		// 设置标题
+		$('hgroup h1').html(item['title']);
+		$('hgroup h2').html(item['artist']);
+
+		// 提示
+		Notification.requestPermission(function (perm) {
+			if (perm == "granted") {
+				var notification = new Notification (item['title'], {
+					dir: "auto",
+					lang: "hi",
+					tag: "SaruFM",
+					icon: item['cover'],
+					body: item['title'] + ' - ' + item['artist']
+				});
+				notification.onshow = function(){
+					setTimeout(function(){
+						notification.close();
+					}, 5000);
+				}
+			}
+		});
+
+		// 开始播放
+		this.audio.play();
+	},
+
+	/* 音乐预加载 */
+	Load: function(i) {
+		var source_dir = {'lossless': 0, 'high': 1, 'normal': 2}, sources = [];
+		playlist[0]['sources'].forEach(function(v, k, c){
+			sources[source_dir[v.quality]] = v;
+			sources[source_dir[v.quality]]['id'] = k;
+		});
+
+		this.Exp.Normal(sources[2]);
+	},
 
 	/* 依赖函数 */
-
-	var extFunct = {
+	extFunc: {
 		randNum: function(min, max) {
 			var radx;
 
@@ -45,7 +234,7 @@ $(document).ready(function(){
 			if ((+max - min) <= 1) return 0;
 
 			// 随机避免和现在重复
-			while ( !radx || radx === SaruData['current'] ) {
+			while ( !radx || radx === this.Current ) {
 				radx = Math.floor(min + Math.random() * (max - min));
 			}
 
@@ -88,244 +277,31 @@ $(document).ready(function(){
 			} // end of for //trans ^ to 0
 			return (_loc8);
 		}
-	};
-
-	/* 播放控制 */
-
-	var SaruControl = {
-		Prev: function() { // 上一首
-			audio.pause();
-
-			if ((+SaruData['current'] + 1) === SaruData['prev'] && SaruData['prev'] > -1) {
-				loadMusic(SaruData['prev']);
-			} else if (+SaruData['current'] == 0){
-				loadMusic(playlist.length - 1);
-			} else if (SaruData['current'] == playlist.length) {
-				loadMusic(SaruData['current']);
-			} else {
-				loadMusic(SaruData['current'] - 1);
-			}
-		},
-
-		Next: function() { // 下一首
-			audio.pause();
-			var nextMusic = 0;
-
-			switch(SaruData['pmode']) {
-				case 0: // 随机播放
-				default:
-					loadMusic(extFunct.randNum(0, playlist.length));
-					break;
-
-				case 1: // 单曲循环
-					audio.currentTime = 0.0;
-					audio.play();
-					break;
-
-				case 2: // 列表顺序
-					if (SaruData['current'] == playlist.length - 1){
-						loadMusic(0);
-					} else {
-						loadMusic(SaruData['current'] + 1);
-					}
-					break;
-			}
-		},
-
-		PlayMode: function() { // 播放顺序
-				if(SaruData['pmode'] == 2){
-					$('.control .repeat i').removeClass(relist[SaruData['pmode']]).addClass(relist[0]).attr('title',retitle[0]);
-					SaruData['pmode'] = localStorage.SaruRepeat = 0;
-				} else {
-					$('.control .repeat i').removeClass(relist[SaruData['pmode']]).addClass(relist[SaruData['pmode'] + 1]).attr('title', retitle[SaruData['pmode'] + 1]);
-					SaruData['pmode'] = localStorage.SaruRepeat = SaruData['pmode'] + 1;
-				}
-		}
-	};
-
-	/* 播放事件 */
-
-	var SaruEvent = {
-		Play: function() { // 播放事件
-			$('#player').addClass('playing');
-			$('.start i').removeClass('fa-play').addClass('fa-pause');
-		},
-
-		Stop: function() { // 暂停事件
-			$('#player').removeClass('playing');
-			$('.start i').removeClass('fa-pause').addClass('fa-play');
-		},
-
-		UpdateProgress: function() { // 进度条更新事件
-			$('#progress .current').css({'width': audio.currentTime / audio.duration * 100 + '%'});
-		},
-
-		End: function() { // 结束事件
-			SaruControl.Next();
-		}
-	};
-
-	/* 音乐品质处理 */
-
-	var SaruExp = {
-		Normal: function(v){
-			if (typeof v.type == undefined)
-				return false;
-
-				switch (v.type) {
-					case 'xiami':
-						$.ajax({
-							type: "GET",
-							cache: false,
-							dataType: 'jsonp',
-							jsonp: 'callback',
-							async: false,
-							url: 'http://www.xiami.com/song/playlist/id/' + v.source +'/object_name/collect/object_id/' + v.source + '/cat/json?_ksTS=1&callback=?',
-							success: function(data){
-								var item = playlist[v.id];
-
-								if (!data)
-									SaruControl.Next();
-
-								if (!item.cover)
-									playlist[v.id]['cover'] = data['data']['trackList'][0]['pic'];
-
-								playMusic({id: v.id, source: extFunc.decodeXiami(data['data']['trackList'][0]['location'])});
-							}
-						});
-						break;
-
-					case '163':
-						AV.initialize("fvyglppby9vbe7x772p7r8ddrxzcq2tcljkxyrf9fo5spbfr", "fspuitd5h1fz3fftya2go8fg293fpwthivno5vjdic2cmwz4");
-						AV.Cloud.run('get163', {id: v.source, type: "song"}, function (result) {
-
-						var data = JSON.parse(result)['songs'][0],
-						item = playlist[v.id];
-
-						if (!data)
-							SaruControl.Next();
-
-						if (!item.cover)
-							playlist[v.id]['cover'] = data['blurPicUrl'];
-
-						playMusic({id: v.id, source: data['mp3Url']});
-					});
-					break;
-
-				case 'file':
-					Saru_ExpTest(v);
-					break;
-			}
-		}
-	};
-
-	/* 音乐格式测试 */
-
-	var Saru_ExpTest = function(v) {
-		playMusic({id: v.id, source: v['source']});
 	}
+};
 
-	/* 音乐播放 */
+$(document).ready(function(){
 
-	var loadMusic = function(i){
-
-		var source_dir = {'lossless': 0, 'high': 1, 'normal': 2}, sources = [];
-		playlist[0]['sources'].forEach(function(v, k, c){
-			sources[source_dir[v.quality]] = v;
-			sources[source_dir[v.quality]]['id'] = k;
-		});
-
-		SaruExp.Normal(sources[2]);
-	}
-
-	var playMusic = function(ritem) {
-		var item = playlist[ritem.id];
-
-		// 记录
-		SaruData['prev']    = localStorage.SaruPrev    = SaruData['current'];
-		SaruData['current'] = localStorage.SaruCurrent = item.id;
-		location.hash = '#!' + item.id;
-
-		// 判断是否喜欢
-		/*if (SaruLike.indexOf(item.id) > -1) {
-			if (!$('.control .like').hasClass('likeit'))
-				$('.control .like').addClass('likeit');
-		} else {
-			if ($('.control .like').hasClass('likeit'))
-				$('.control .like').removeClass('likeit');
-		}*/
-
-		audio.setAttribute("src", ritem['source']);
-		audio.addEventListener('play',  SaruEvent.Play, false);
-		audio.addEventListener('pause', SaruEvent.Stop, false);
-		audio.addEventListener('timeupdate', SaruEvent.UpdateProgress, false);
-		audio.addEventListener('ended', SaruEvent.End, false);
-
-		// 设置封面
-		cover = item['cover'] ? item['cover'] : 'img/album.jpg';
-
-		$('#cover').attr({
-			'style': 'background-image:url(' + cover + ');',
-			'title': item['title'] + ' - ' + item['artist']
-		});
-
-		// 设置标题
-		$('hgroup h1').html(item['title']);
-		$('hgroup h2').html(item['artist']);
-
-		// 播放列表
-		$('.play-list ul li').removeClass('playing').eq(i).addClass('playing');
-
-		// 提示
-		Notification.requestPermission(function (perm) {
-			if (perm == "granted") {
-				var notification = new Notification (item['title'], {
-					dir: "auto",
-					lang: "hi",
-					tag: "SaruFM",
-					icon: item['cover'],
-					body: item['title'] + ' - ' + item['artist']
-				});
-				notification.onshow = function(){
-					setTimeout(function(){
-						notification.close();
-					}, 5000);
-				}
-			}
-		});
-
-		// 开始播放
-		audio.play();
-	}
-
-	if (location.hash.match(/[\d]+/)) {
-		var hash = location.hash.match(/[\d]+/)[0];
-		if (hash < playlist.length)
-			loadMusic(hash);
-	} else if (localStorage.SaruPrev > -1 && localStorage.SaruPrev < playlist.length) {
-		loadMusic(localStorage.SaruPrev);
-	} else {
-		loadMusic(extFunct.randNum(0,playlist.length));
-	}
+	Saru.Init();
 
 	$('.center').click(function() {
 		if ($('#player').hasClass('playing')){
-			audio.pause();
+			Saru.audio.pause();
 		} else {
-			audio.play();
+			Saru.audio.play();
 		}
 	});
 
 	$('.control .prev').click(function(){
-		SaruControl.Prev();
+		Saru.Control.Prev();
 	});
 
 	$('.control .next').click(function(){
-		SaruControl.Next();
+		Saru.Control.Next();
 	});
 
 	$('.control .repeat').click(function(){
-		SaruControl.PlayMode();
+		Saru.Control.PlayMode();
 	});
 
 	$('.control .like').click(function(){
@@ -339,24 +315,24 @@ $(document).ready(function(){
 		switch (keycode) {
 			case 39:
 			case 78: // 右键, n键; 下一首
-				SaruControl.Next();
+				Saru.Control.Next();
 				break;
 
 			case 37: // 左键; 上一首
-				SaruControl.Prev();
+				Saru.Control.Prev();
 				break;
 
 			case 80:
 			case 32: // P键, 空格键; 播放暂停
 				if ($('#player').hasClass('playing')){
-					audio.pause();
+					Saru.audio.pause();
 				} else {
-					audio.play();
+					Saru.audio.play();
 				}
 				break;
 
 			case 77: //M键; 播放顺序
-				SaruControl.PlayMode();
+				Saru.Control.PlayMode();
 				break;
 
 			case 76: //L键; 喜欢; 不喜欢
